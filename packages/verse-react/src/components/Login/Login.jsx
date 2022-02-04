f7be9cebf4bdb0d09a26c6f18f02d9b8015a609e
Krishna <shankarkrishna26@gmail.com>
2022-02-04 21:23:00 +0530

    login props change

--- packages/verse-react/src/components/Login/Login.jsx
+++ packages/verse-react/src/components/Login/Login.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
+import { useForm } from '../../utility/hooks/useModel';
 import { Button } from '../Button';
 import { Form } from '../Form';
 
-export const Login = () => {
+export const Login = ({ onLogin }) => {
+  const loginData = useForm('login');
+
+  const onLoginClick = () => {
+    onLogin(loginData);
+  };
+
   return (
     <Form className="login flex-center p-x-16 m-x-8">
       <Form.Wrapper className="flex-wrap flex-center gutter p-x-32">
@@ -17,8 +26,18 @@ export const Login = () => {
         />
         <Form.Label>DOB</Form.Label>
         <Form.Input model="login.dob" placeholder="DOB" />
-        <Button className="btn-block secondary m-t-16">Login</Button>
+        <Button className="btn-block secondary m-t-16" onClick={onLoginClick}>
+          Login
+        </Button>
       </Form.Wrapper>
     </Form>
   );
+};
+
+Login.propTypes = {
+  onLogin: PropTypes.func,
+};
+
+Login.defaultProps = {
+  onLogin: () => null,
 };
