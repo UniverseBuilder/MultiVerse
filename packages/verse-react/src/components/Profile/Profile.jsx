9a50293d7c2d2a67857397b9f5f71e114f117bbf
Krishna <shankarkrishna26@gmail.com>
2022-02-07 22:35:23 +0530

    add profile click

--- packages/verse-react/src/components/Profile/Profile.jsx
+++ packages/verse-react/src/components/Profile/Profile.jsx
@@ -5,12 +5,13 @@ import PropTypes from 'prop-types';
 import { useModel } from '../../utility/hooks';
 import { useForm } from '../../utility/redux/slices/forms/formSlice';
 
-export const Profile = ({ model, children }) => {
+export const Profile = ({ model, onClick, children }) => {
   const showContent = useModel(model);
   const { set } = useForm();
 
   const handleProfileClick = () => {
     set({ model, value: !showContent });
+    onClick(showContent);
   };
 
   const childrenWithProps = React.Children.map(children, child => {
@@ -29,9 +30,11 @@ export const Profile = ({ model, children }) => {
 Profile.propTypes = {
   children: PropTypes.node.isRequired,
   model: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 Profile.defaultProps = {
   controlled: false,
   model: 'profile',
+  onClick: () => null,
 };
