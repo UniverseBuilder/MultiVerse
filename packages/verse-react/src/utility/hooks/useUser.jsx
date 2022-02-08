71fe1e8ace932992d4c382677f078f061b1cda0a
Krishna <shankarkrishna26@gmail.com>
2022-02-08 13:36:27 +0530

    update useUser

--- packages/verse-react/src/utility/hooks/useUser.jsx
+++ packages/verse-react/src/utility/hooks/useUser.jsx
@@ -5,7 +5,7 @@ export const useUser = () => {
   let { login } = useApiState('login');
 
   if (!login) {
-    login = sessionStorage.get('login');
+    login = sessionStorage.get('login')?.data;
   }
 
   return login;
